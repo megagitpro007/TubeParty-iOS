c7178d5b01238ecfb3a081c2e0588738d0e3d483
Chaiwat Ungpratansombat <ice@appsynth.net>
2022-06-25 22:12:24 +0700

    initial extension uicolor

---
+++ TubeParty/Utilities/UIColor+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  TubeParty
+//
+//  Created by iZE Appsynth on 25/6/2565 BE.
+//
+
+import Foundation
+import UIKit
+
+public extension UIColor {
+    static let systemMainGreen = #colorLiteral(red: 0.4980392157, green: 0.6431372549, blue: 0.462745098, alpha: 1)
+}
