6e7b8bce19958798d40e279393d75757b1db08df
Chaiwat Ungpratansombat <ice@appsynth.net>
2022-06-26 16:11:20 +0700

    update app color

--- TubeParty/Extensions/UIColor+Extensions.swift
+++ TubeParty/Extensions/UIColor+Extensions.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 public extension UIColor {
-    static let systemMainGreen = #colorLiteral(red: 0.4980392157, green: 0.6431372549, blue: 0.462745098, alpha: 1)
+    static let systemMainGreen = #colorLiteral(red: 0.2672426701, green: 0.6865548491, blue: 0.5144678354, alpha: 1)
     static let systemBlackButton = #colorLiteral(red: 0.2006945312, green: 0.2006945312, blue: 0.2006945312, alpha: 1)
     static let systemGrayButton = #colorLiteral(red: 0.7709764838, green: 0.7709764838, blue: 0.7709764838, alpha: 1)
 }
