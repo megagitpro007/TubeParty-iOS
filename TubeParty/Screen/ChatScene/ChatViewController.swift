cfaf7b5040d7101e84b2dda9d715aa80a970ee04
Chaiwat Ungpratansombat <ice@appsynth.net>
2022-07-05 22:30:01 +0700

    feat: [TP-N/A] - refactor data handler to RxDataSource

--- TubeParty/Screen/ChatScene/ChatViewController.swift
+++ TubeParty/Screen/ChatScene/ChatViewController.swift
@@ -24,12 +24,9 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var textFieldContainer: UIView!
     @IBOutlet weak var bottomViewContainer: UIView!
     
-    let textList = ["message1", "message2", "message3", "message4","message5", "message6", "message7", "message8","message9", "ReceiverViewCell", "youtube https://www.youtube.com/watch?v=dRfafqSvoF8&ab_channel=UAStudios",
-                    "Prettymuch https://www.prettymuch.com/ ",
-                    "google https://www.facebook.com"]
-    
     var isTextFieldSelected: Bool = false
     
+    var dataSource: RxTableViewSectionedReloadDataSource<SectionModel>!
     var viewModel: ChatIOType!
     let bag = DisposeBag()
     
@@ -42,59 +39,12 @@ class ChatViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let indexPath = IndexPath(row: self.textList.count-1, section: 0)
-        self.chatTableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
-    }
-    
-    func bindViewModel() {
-        //input
-        viewModel.input.viewDidload.accept(())
-        
-        typingField
-            .rx
-            .text.orEmpty.map({ text in
-                return !text.isEmpty
-            })
-            .bind(to: viewModel.input.isValidText).disposed(by: bag)
-        
-        //output
-        viewModel.output.isDisableSendButton.distinctUntilChanged().drive(onNext: { isDisable in
-            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut) { [weak self] in
-                guard let self = self else { return }
-                print("isDisable : \(isDisable)")
-                self.sendButton.isUserInteractionEnabled = isDisable
-                self.sendButton.isEnabled = isDisable
-            }
-        }).disposed(by: bag)
-        
-        NotificationCenter
-            .default
-            .rx
-            .notification(.keyboardWillShow).bind { [weak self] notification in
-                guard let self = self else { return }
-                guard self.isTextFieldSelected else { return }
-                if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-                    let size = keyboardSize.height - self.view.safeAreaInsets.bottom
-                    self.bottomViewContainer.transform = .init(translationX: 0, y: -size)
-                    self.chatTableView.contentInset = .init(top: 0, left: 0, bottom: size + 10, right: 0)
-                    
-                    let indexPath = IndexPath(row: self.textList.count-1, section: 0)
-                    self.chatTableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
-                    
-                }
-                print("🔥 : textFieldDidBeginEditing")
-            }.disposed(by: bag)
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action:#selector(dismissKeyboard))
-        view.addGestureRecognizer(tapGesture)
-        tapGesture.rx.event.bind(onNext: { recognizer in
-            // TODO:
-        }).disposed(by: bag)
-        
+//        let indexPath = IndexPath(row: self.textList.count-1, section: 0)
+//        self.chatTableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
     }
     
     func registerCell() {
-        chatTableView.dataSource = self
+//        chatTableView.dataSource = self
         chatTableView.register(UINib(nibName: senderTableViewCell, bundle: nil), forCellReuseIdentifier: senderTableViewCell)
         chatTableView.register(UINib(nibName: receiverTableViewCell, bundle: nil), forCellReuseIdentifier: receiverTableViewCell)
     }
@@ -133,6 +83,82 @@ class ChatViewController: UIViewController {
         
         typingField.backgroundColor = .clear
         textFieldContainer.backgroundColor = .tpArsenic
+    }
+    
+    func bindViewModel() {
+        //input
+        viewModel.input.viewDidload.accept(())
+        
+        typingField
+            .rx
+            .text.orEmpty.map({ text in
+                return !text.isEmpty
+            })
+            .bind(to: viewModel.input.isValidText).disposed(by: bag)
+        
+        //output
+        viewModel.output.isDisableSendButton.distinctUntilChanged().drive(onNext: { isDisable in
+            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut) { [weak self] in
+                guard let self = self else { return }
+                print("isDisable : \(isDisable)")
+                self.sendButton.isUserInteractionEnabled = isDisable
+                self.sendButton.isEnabled = isDisable
+            }
+        }).disposed(by: bag)
+        
+        NotificationCenter
+            .default
+            .rx
+            .notification(.keyboardWillShow).bind { [weak self] notification in
+                guard let self = self else { return }
+                guard self.isTextFieldSelected else { return }
+                if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                    let size = keyboardSize.height - self.view.safeAreaInsets.bottom
+                    self.bottomViewContainer.transform = .init(translationX: 0, y: -size)
+                    self.chatTableView.contentInset = .init(top: 0, left: 0, bottom: size + 10, right: 0)
+                    
+//                    let indexPath = IndexPath(row: self.textList.count-1, section: 0)
+//                    self.chatTableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+                    
+                }
+                print("🔥 : textFieldDidBeginEditing")
+            }.disposed(by: bag)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action:#selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
+        tapGesture.rx.event.bind(onNext: { recognizer in
+            // TODO:
+        }).disposed(by: bag)
+        
+        //Datasource
+        dataSource = RxTableViewSectionedReloadDataSource<SectionModel>(configureCell: { [weak self] (datasource, tableview, indexpath, item) -> UITableViewCell in
+            guard let self = self else { return UITableViewCell() }
+            let cell = self.chatTableView.dequeueReusableCell(withIdentifier: indexpath.row%2 == 0 ? senderTableViewCell : receiverTableViewCell, for: indexpath)
+            if let cell = cell as? SenderViewCell {
+                cell.configure(
+                    name: item.profileName,
+                    text: item.message,
+                    url: item.profileURL.absoluteString,
+                    timeStamp: item.timeStamp,
+                    linkPreview: item.message.formatURL()
+                )
+                
+            } else if let cell = cell as? ReceiverViewCell {
+                cell.configure(
+                    name: item.profileName,
+                    text: item.message,
+                    url: item.profileURL.absoluteString,
+                    timeStamp: item.timeStamp,
+                    linkPreview: item.message.formatURL()
+                )
+            }
+            return cell
+        })
+        
+        viewModel.output.getChatMessage
+            .map{[$0]}
+            .drive(chatTableView.rx.items(dataSource: dataSource))
+            .disposed(by: bag)
         
     }
     
@@ -143,49 +169,13 @@ class ChatViewController: UIViewController {
             self.chatTableView.transform = .identity
             
             self.chatTableView.contentInset = .init(top: 0, left: 0, bottom: 0, right: 0)
-            let indexPath = IndexPath(row: self.textList.count-1, section: 0)
-            self.chatTableView.scrollToRow(at: indexPath, at: .top, animated: true)
+//            let indexPath = IndexPath(row: self.textList.count-1, section: 0)
+//            self.chatTableView.scrollToRow(at: indexPath, at: .top, animated: true)
             self.view.endEditing(true)
             
         })
     }
 
-}
-
-extension ChatViewController: UITableViewDataSource {
-    
-    // Tableview Delegate
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        textList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = chatTableView.dequeueReusableCell(withIdentifier: indexPath.row%2 == 0 ? senderTableViewCell : receiverTableViewCell, for: indexPath)
-        if let cell = cell as? SenderViewCell {
-            // Should to be `model`
-            let text = textList[indexPath.row]
-            cell.configure(
-                name: "ice",
-                text: text,
-                url: "https://static.wikia.nocookie.net/love-exalted/images/1/1c/Izuku_Midoriya.png/revision/latest?cb=20211011173004",
-                timeStamp: "11:11",
-                linkPreview: text.formatURL()
-            )
-            
-        } else if let cell = cell as? ReceiverViewCell {
-            let text = textList[indexPath.row]
-            cell.configure(
-                name: "toney",
-                text: text,
-                url: "https://nntheblog.b-cdn.net/wp-content/uploads/2022/04/Arrangement-Katsuki-Bakugo.jpg",
-                timeStamp: "11:11",
-                linkPreview: text.formatURL()
-            )
-        }
-        
-        return cell
-    }
-    
 }
 
 extension ChatViewController: UITextFieldDelegate {
--- TubeParty/Screen/ChatScene/ChatViewModel.swift
+++ TubeParty/Screen/ChatScene/ChatViewModel.swift
@@ -45,25 +45,25 @@ class ChatViewModel: ChatIOType, ChatInput, ChatOutput {
     
     // Properties
     var _isDisableSendButton: BehaviorRelay<Bool> = .init(value: true)
-    var _getChatMessage: PublishRelay<SectionModel> = .init()
+    var _getChatMessage: BehaviorRelay<SectionModel> = .init(value: SectionModel(header: "", items: []))
     
-    let chatList = [MessageModel(profileName: "Toney",
-                                 profileURL: "https://nntheblog.b-cdn.net/wp-content/uploads/2022/04/Arrangement-Katsuki-Bakugo.jpg",
-                                 message: "message1",
-                                 timeStamp: "11:11"),
-                    
-                    MessageModel(profileName: "ize",
+    let chatList = [MessageModel(profileName: "ize",
                                  profileURL: "https://static.wikia.nocookie.net/love-exalted/images/1/1c/Izuku_Midoriya.png/revision/latest?cb=20211011173004",
-                                 message: "message2",
+                                 message: "message1",
                                  timeStamp: "11:11"),
                     
                     MessageModel(profileName: "Toney",
                                  profileURL: "https://nntheblog.b-cdn.net/wp-content/uploads/2022/04/Arrangement-Katsuki-Bakugo.jpg",
-                                 message: "message3",
+                                 message: "message2",
                                  timeStamp: "11:11"),
                     
                     MessageModel(profileName: "ize",
                                  profileURL: "https://static.wikia.nocookie.net/love-exalted/images/1/1c/Izuku_Midoriya.png/revision/latest?cb=20211011173004",
+                                 message: "message3",
+                                 timeStamp: "11:11"),
+                    
+                    MessageModel(profileName: "Toney",
+                                 profileURL: "https://nntheblog.b-cdn.net/wp-content/uploads/2022/04/Arrangement-Katsuki-Bakugo.jpg",
                                  message: "message4 Prettymuch https://www.prettymuch.com/ ",
                                  timeStamp: "11:11")]
     
@@ -72,14 +72,14 @@ class ChatViewModel: ChatIOType, ChatInput, ChatOutput {
     init(userChatName: String) {
         
         isValidText
-            .map({ $0 })
+            .map({$0})
             .bind(to: _isDisableSendButton)
             .disposed(by: bag)
         
         viewDidload.map { [weak self] _ -> SectionModel in
-            
+
             guard let self = self else { return SectionModel(header: "", items: []) }
-            
+
             var messageModel: [MessageModel] = []
 
             for data in self.chatList {
@@ -88,9 +88,9 @@ class ChatViewModel: ChatIOType, ChatInput, ChatOutput {
                                                  message: data.message,
                                                  timeStamp: data.timeStamp))
             }
-            
             return SectionModel(header: "ChatMessageHeader", items: messageModel)
         }
+
         .bind(to: _getChatMessage)
         .disposed(by: bag)
         
